46d834122e24367c3587005872399eb5d723d2a5
stratosphr <stratosphr@gmail.com>
2018-07-04 22:29:37 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import geometry.Vector;
+import gui.MainPanel;
+import gui.MainWindow;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MainWindow mainWindow = new MainWindow("Test", new Vector(800, 600), new MainPanel());
+        mainWindow.setFullScreen();
+        mainWindow.open();
+        mainWindow.minimize();
+        mainWindow.close();
+    }
+
+}
